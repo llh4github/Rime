83a1dd401134c2cc01d224d587544236cce6f6f7
llh4github <lilinhong_coding@foxmail.com>
2025-01-09 23:07:49 +0800

    fix: 在处理Ctrl+左括号时清空输入内容并更新注释

--- lua/handle_ctrl_bracket_processor.lua
+++ lua/handle_ctrl_bracket_processor.lua
@@ -13,10 +13,11 @@ function handle_ctrl_bracket_processor.func(key, env)
     local is_ascii_mode = ctx:get_option("ascii_mode")
     if (key:repr() == "Control+bracketleft" and (not is_ascii_mode)) then
         ctx:set_option("ascii_mode", true)
+        -- 清空输入内容
         env.engine:commit_text()
-        return 1
+        return 1 -- 表示已处理
     end
-    return 2 -- 表示未处理
+    return 2     -- 表示未处理
 end
 
 return handle_ctrl_bracket_processor
