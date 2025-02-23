ddd607f002366d86405532bf9561f65b2b393cd1
llh4github <lilinhong_coding@foxmail.com>
2025-02-23 19:36:49 +0800

    fix: 组合键继续向后传递

--- lua/handle_ctrl_bracket_processor.lua
+++ lua/handle_ctrl_bracket_processor.lua
@@ -11,12 +11,11 @@ end
 function handle_ctrl_bracket_processor.func(key, env)
     local ctx = env.engine.context
     local is_ascii_mode = ctx:get_option("ascii_mode")
-    if (key:repr() == "Control+bracketleft" and (not is_ascii_mode)) then
+    if ((not is_ascii_mode) and key:repr() == "Control+bracketleft") then
         ctx:set_option("ascii_mode", true)
         -- 清空输入内容
         -- env.engine:clear()
         ctx.input = ''
-        return 1 -- 表示已处理
     end
     return 2     -- 表示未处理
 end
