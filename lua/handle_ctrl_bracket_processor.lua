a5618c0fa59b1407a43773502047b4da498d368d
llh4github <61351434+llh4github@users.noreply.github.com>
2025-02-23 19:40:14 +0800

    feat: 适配vim模式转化组合键 (#1)
    * feat: 添加处理vim回普通模式的快捷键处理器
    
    * fix: 更新注释以说明vim回普通模式时清空rime输入内容
    
    * fix: 在处理Ctrl+左括号时清空输入内容并更新注释
    
    * fix: 默认开启ascii_mode
    
    * fix: 处理器注册与清空提交
    
    * fix: 组合键继续向后传递

---
+++ lua/handle_ctrl_bracket_processor.lua
@@ -0,0 +1,23 @@
+-- 处理vim回普通模式的快捷键
+-- vim 回普通模式时 切为英文状态，会清空rime的输入内容
+-- 只处理了 ctrl [  组合键
+local handle_ctrl_bracket_processor = {}
+
+function handle_ctrl_bracket_processor.init(env)
+    -- 初始化代码
+    print("处理器初始化")
+end
+
+function handle_ctrl_bracket_processor.func(key, env)
+    local ctx = env.engine.context
+    local is_ascii_mode = ctx:get_option("ascii_mode")
+    if ((not is_ascii_mode) and key:repr() == "Control+bracketleft") then
+        ctx:set_option("ascii_mode", true)
+        -- 清空输入内容
+        -- env.engine:clear()
+        ctx.input = ''
+    end
+    return 2     -- 表示未处理
+end
+
+return handle_ctrl_bracket_processor
